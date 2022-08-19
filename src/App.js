2d2e5c146fae219629e12a2dcec5e0b8462dc8ab
Tito Sanchez <99434691+titothetrader@users.noreply.github.com>
2022-08-18 22:18:58 -0400

    init commit
    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Cytoscape from './Cytoscape';
+
+function App() {
+  return (
+    <div>
+      <h1>Cytoscape React Test!</h1>
+      <Cytoscape />
+    </div>
+  );
+}
+
+export default App;
